c507184a7c329948a9a6192e694d3359cfd74151
Rui Vieira <revs87@msn.com>
2024-06-11 18:04:24 +0100

    iOS structure fix

---
+++ composeApp/src/iosMain/kotlin/com/rvcoding/pedropeapp/domain/player/Player.ios.kt
@@ -0,0 +1,5 @@
+package com.rvcoding.pedropeapp.domain.player
+
+actual fun getPlayer(): Player {
+    TODO("Not yet implemented")
+}
--- composeApp/src/iosMain/kotlin/domain/player/Player.ios.kt
+++
@@ -1,7 +0,0 @@
-package domain.player
-
-import com.rvcoding.pedropeapp.domain.player.Player
-
-actual fun getPlayer(): Player {
-    TODO("Not yet implemented")
-}
