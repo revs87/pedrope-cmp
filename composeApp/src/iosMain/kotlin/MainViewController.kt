e203b2e8e5d16992b45a95068190a9a4ff197b8c
Rui Vieira <revs87@msn.com>
2024-06-12 06:41:36 +0100

    iOS Dark mode set

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,4 +1,17 @@
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.ComposeUIViewController
 import com.rvcoding.pedropeapp.App
+import platform.UIKit.UIApplication
+import platform.UIKit.UIUserInterfaceStyle
+import platform.UIKit.UIWindow
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    LaunchedEffect(Unit) { setStatusBarStyle() }
+    App()
+}
+
+private fun setStatusBarStyle() {
+    val window = UIApplication.sharedApplication.windows.firstOrNull() as? UIWindow
+    val rootViewController = window?.rootViewController
+    rootViewController?.view?.window?.overrideUserInterfaceStyle = UIUserInterfaceStyle.UIUserInterfaceStyleDark
+}
