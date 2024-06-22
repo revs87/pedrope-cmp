eb4f33faa295a8abac7a27dee6c18bcfb37d8998
Rui Vieira <revs87@msn.com>
2024-06-22 01:29:43 +0100

    Image rotation animation added

--- composeApp/src/commonMain/kotlin/com/rvcoding/pedropeapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/rvcoding/pedropeapp/App.kt
@@ -1,5 +1,11 @@
 package com.rvcoding.pedropeapp
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,9 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -21,17 +29,11 @@ import coil3.compose.setSingletonImageLoaderFactory
 import coil3.request.crossfade
 import coil3.util.DebugLogger
 import com.rvcoding.pedropeapp.presentation.VideoScreen
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalCoilApi::class, ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
-    setSingletonImageLoaderFactory { context ->
-        getAsyncImageLoader(context)
-    }
-
     MaterialTheme {
         Box(
             modifier = Modifier
@@ -48,14 +50,40 @@ fun App() {
                 VideoScreen()
             }
 
-            SubcomposeAsyncImage(
-                modifier = Modifier.padding(12.dp).size(64.dp),
-                model = "https://yt3.googleusercontent.com/BAC0XLkL-GL6oq1MFWRRxV7GqJMZC7vbh0Q6QMg753E6oJZngV33QQ6cvAnZWLB5OOx9nOh7KSQ=s900-c-k-c0x00ffffff-no-rj",
-                contentDescription = "Raccoon Image",
+            RotatingImage(
+                imageUrl = "https://yt3.googleusercontent.com/BAC0XLkL-GL6oq1MFWRRxV7GqJMZC7vbh0Q6QMg753E6oJZngV33QQ6cvAnZWLB5OOx9nOh7KSQ=s900-c-k-c0x00ffffff-no-rj"
             )
-
         }
     }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun RotatingImage(imageUrl: String) {
+    setSingletonImageLoaderFactory { context ->
+        getAsyncImageLoader(context)
+    }
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val angle by infiniteTransition.animateFloat(
+        initialValue = 360f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 5500, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    SubcomposeAsyncImage(
+        model = imageUrl,
+        contentDescription = imageUrl.takeLast(5),
+        modifier = Modifier
+            .padding(12.dp)
+            .size(64.dp)
+            .graphicsLayer {
+                rotationZ = angle
+            }
+    )
 }
 
 fun getAsyncImageLoader(context: PlatformContext) =
